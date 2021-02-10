2b85aa3225f7bbada0611852faea612d9879c8a4
Joan Currie <jecurrie8@gmail.com>
2021-02-10 13:59:25 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import turtle
+import turtle as t
+import random
 
-polygon = turtle.Turtle()
+polygon = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 for i in range(3, 10):
     num_sides = i
     side_length = 70
     angle = 360.0 / num_sides
-
+    polygon.color(random.choice(colours))
     for i in range(num_sides):
+
         polygon.forward(side_length)
         polygon.right(angle)
 
 
-turtle.done()
